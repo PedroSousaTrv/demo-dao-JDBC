c5a00945f1c18f1b4ecf4739b0cd460ec4e26e09
Pedro de Sousa <pedrosousa2003@gmail.com>
2026-01-03 18:43:25 -0300

    Seller entity class

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,12 +1,19 @@
 package Aplication;
 
 import Model.Entities.Department;
+import Model.Entities.Seller;
+import com.mysql.cj.protocol.a.LocalDateTimeValueEncoder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Program {
     public static void main(String[] args) {
 
         Department d1 = new Department(1,"Books");
+        Seller seller = new Seller(21,"pedro","pedro@gmail.com", LocalDateTime.now(),300.0,d1);
         System.out.println(d1);
+        System.out.println(seller);
 
     }
 }
